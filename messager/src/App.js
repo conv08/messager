2693cb03531b41f418ad6bf84316005699f5944a
Bastian Larsen <bastian.larsen08@outlook.de>
2024-01-24 22:01:17 +0100

    Removed React defaults

--- messager/src/App.js
+++ messager/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return(
+        <div className="App">
+            <body className="App-Body">
+                <input placeholder="Please enter Message" className="InField" type="text" id="msg" name="msg"></input>
+            </body>
+        </div>
+    );
 }
 
 export default App;
